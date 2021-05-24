2430687283b1f60220763ad923700c29d77680a8
chunhochan <chanc3@oregonstate.edu>
2021-05-25 06:20:47 +0800

    new alienmovement

---
+++ Temp/il2cppOutput/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,203 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Bullet::Start()
+extern void Bullet_Start_m58181B46F80FE1ABECE1AFF455C253B51B7EB0E4 (void);
+// 0x00000002 System.Void Bullet::Update()
+extern void Bullet_Update_mB82408CA535D8533168045E7C0321090448B596B (void);
+// 0x00000003 System.Void Bullet::.ctor()
+extern void Bullet__ctor_mC7D931FE508342F413FBA79525A4819D4114B3EC (void);
+// 0x00000004 System.Void ShipShootScript::Start()
+extern void ShipShootScript_Start_m8EE6BBC39D6F2099DE60C9DD28F5A515C397060D (void);
+// 0x00000005 System.Void ShipShootScript::Update()
+extern void ShipShootScript_Update_m045AC08700050E8A405D3B075DE45462A55D27FD (void);
+// 0x00000006 System.Void ShipShootScript::.ctor()
+extern void ShipShootScript__ctor_mB0773F25B6ED179475375887B5F911B8409EAD48 (void);
+// 0x00000007 System.Void MoveCamera::moveLeft()
+extern void MoveCamera_moveLeft_m3A2C1D260EE8ECB843446F81F91B9C8EADB260EC (void);
+// 0x00000008 System.Void MoveCamera::moveRight()
+extern void MoveCamera_moveRight_m4FC9ED7208C715010F0C343315941C735A8FB016 (void);
+// 0x00000009 System.Void MoveCamera::.ctor()
+extern void MoveCamera__ctor_m6BDEAC8FE0ECABD5CF28A49353BC8F9132AF5A61 (void);
+// 0x0000000A System.Void NewBehaviourScript::Start()
+extern void NewBehaviourScript_Start_m783F84A617DADC4574B0BF1524481E6B96C65661 (void);
+// 0x0000000B System.Collections.IEnumerator NewBehaviourScript::StartSpawning()
+extern void NewBehaviourScript_StartSpawning_mE0118269A4909CF62DAF24DC555FD5E1CC838BF3 (void);
+// 0x0000000C System.Void NewBehaviourScript::.ctor()
+extern void NewBehaviourScript__ctor_m437970EA37D66BDF32972F4CC0F65B95E5961FAA (void);
+// 0x0000000D System.Void NewBehaviourScript/<StartSpawning>d__3::.ctor(System.Int32)
+extern void U3CStartSpawningU3Ed__3__ctor_m8695317A420747B040FF72F7E1AE285F1FB45CE1 (void);
+// 0x0000000E System.Void NewBehaviourScript/<StartSpawning>d__3::System.IDisposable.Dispose()
+extern void U3CStartSpawningU3Ed__3_System_IDisposable_Dispose_mF89F63A9106D520B6A6A3DAFC0F8EF402A11451A (void);
+// 0x0000000F System.Boolean NewBehaviourScript/<StartSpawning>d__3::MoveNext()
+extern void U3CStartSpawningU3Ed__3_MoveNext_m8BED59DD8ABF505C9A4886C7EE42A474EF6C8878 (void);
+// 0x00000010 System.Object NewBehaviourScript/<StartSpawning>d__3::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CStartSpawningU3Ed__3_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mCE9165AFAC7075C14B1BDD2F3051E4BFA04414C8 (void);
+// 0x00000011 System.Void NewBehaviourScript/<StartSpawning>d__3::System.Collections.IEnumerator.Reset()
+extern void U3CStartSpawningU3Ed__3_System_Collections_IEnumerator_Reset_mC4AE2C0BEC87E85386558AF30BFC67AB24A4B606 (void);
+// 0x00000012 System.Object NewBehaviourScript/<StartSpawning>d__3::System.Collections.IEnumerator.get_Current()
+extern void U3CStartSpawningU3Ed__3_System_Collections_IEnumerator_get_Current_m748B28AA1E9B5BC69E1AD65379123EFB95F646C8 (void);
+// 0x00000013 System.Void Player::Start()
+extern void Player_Start_mBD692B64AAC791B93A589E7D3596F36787EAF021 (void);
+// 0x00000014 System.Void Player::Update()
+extern void Player_Update_mBA04F1D6FE3C18037EA95DFAEEAA9977BFD49CD3 (void);
+// 0x00000015 System.Void Player::.ctor()
+extern void Player__ctor_mDE8EB5B351975D4E7E24DE341B8B49B8A29CC2B7 (void);
+// 0x00000016 System.Void ShootScript::Shoot()
+extern void ShootScript_Shoot_mFF81D242F47F7059D0BFFA17D0C824C3649A5602 (void);
+// 0x00000017 System.Void ShootScript::.ctor()
+extern void ShootScript__ctor_m9D99A2EE188B26D935ECED4A5B730EC793BA96C0 (void);
+// 0x00000018 System.Void alienattack::Start()
+extern void alienattack_Start_m7D535320E23183DB7727479321721A549C08CF83 (void);
+// 0x00000019 System.Collections.IEnumerator alienattack::Startshooting()
+extern void alienattack_Startshooting_m4A6C20966B441EF8D5A8B773B177574CB0944B55 (void);
+// 0x0000001A System.Void alienattack::.ctor()
+extern void alienattack__ctor_m01ACCA1F66639AF0DFA2B7176D91230BA55C2F15 (void);
+// 0x0000001B System.Void alienattack/<Startshooting>d__3::.ctor(System.Int32)
+extern void U3CStartshootingU3Ed__3__ctor_m15DF1F595204E9689959739C988D65C61223D28F (void);
+// 0x0000001C System.Void alienattack/<Startshooting>d__3::System.IDisposable.Dispose()
+extern void U3CStartshootingU3Ed__3_System_IDisposable_Dispose_m34CDB6211F4104FAC4F13C14493ECF08F6035E27 (void);
+// 0x0000001D System.Boolean alienattack/<Startshooting>d__3::MoveNext()
+extern void U3CStartshootingU3Ed__3_MoveNext_m4BF8AAA5CFE4F3406B95B534EA0F32A018DE61E2 (void);
+// 0x0000001E System.Object alienattack/<Startshooting>d__3::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CStartshootingU3Ed__3_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m79CCE0C7F3425D447A39F9AE793908B79AA31813 (void);
+// 0x0000001F System.Void alienattack/<Startshooting>d__3::System.Collections.IEnumerator.Reset()
+extern void U3CStartshootingU3Ed__3_System_Collections_IEnumerator_Reset_m870DF1A140BBA5FD81DFE27A0AA567C4DC341433 (void);
+// 0x00000020 System.Object alienattack/<Startshooting>d__3::System.Collections.IEnumerator.get_Current()
+extern void U3CStartshootingU3Ed__3_System_Collections_IEnumerator_get_Current_m1F4F6EE6806670FC7D576804FA3A0C0EC0BF1AB9 (void);
+// 0x00000021 System.Void balloonScript::Start()
+extern void balloonScript_Start_m20282F79FCA5561E8E7CEBEECA98F354E5A3491A (void);
+// 0x00000022 System.Void balloonScript::Update()
+extern void balloonScript_Update_mFB0AB5309CBA7BED8D8AD34B39499217E71BA094 (void);
+// 0x00000023 System.Void balloonScript::.ctor()
+extern void balloonScript__ctor_m706012D88FF5C005F9AAE2313E5DD0BE4800BA27 (void);
+// 0x00000024 System.Void fightingbullet::Start()
+extern void fightingbullet_Start_mAEC96BE9CCD048A9CF914BB2DC9ECBE7D1430CE3 (void);
+// 0x00000025 System.Void fightingbullet::Update()
+extern void fightingbullet_Update_mCBDA5295DC4EF361D2C655F6AC91D2A8C8D3DF8D (void);
+// 0x00000026 System.Void fightingbullet::.ctor()
+extern void fightingbullet__ctor_m8BAF23C795D61AC68C64F94C6C85DD8608C6A1C3 (void);
+// 0x00000027 System.Void ReadmeStarSparrow::.ctor()
+extern void ReadmeStarSparrow__ctor_m81EE107F0BDBF341ABE7355F3A3C5CA3E5983209 (void);
+// 0x00000028 System.Void ReadmeStarSparrow/Section::.ctor()
+extern void Section__ctor_mA5437646B1723B160F8C957D956245BF25C3D7E4 (void);
+static Il2CppMethodPointer s_methodPointers[40] = 
+{
+	Bullet_Start_m58181B46F80FE1ABECE1AFF455C253B51B7EB0E4,
+	Bullet_Update_mB82408CA535D8533168045E7C0321090448B596B,
+	Bullet__ctor_mC7D931FE508342F413FBA79525A4819D4114B3EC,
+	ShipShootScript_Start_m8EE6BBC39D6F2099DE60C9DD28F5A515C397060D,
+	ShipShootScript_Update_m045AC08700050E8A405D3B075DE45462A55D27FD,
+	ShipShootScript__ctor_mB0773F25B6ED179475375887B5F911B8409EAD48,
+	MoveCamera_moveLeft_m3A2C1D260EE8ECB843446F81F91B9C8EADB260EC,
+	MoveCamera_moveRight_m4FC9ED7208C715010F0C343315941C735A8FB016,
+	MoveCamera__ctor_m6BDEAC8FE0ECABD5CF28A49353BC8F9132AF5A61,
+	NewBehaviourScript_Start_m783F84A617DADC4574B0BF1524481E6B96C65661,
+	NewBehaviourScript_StartSpawning_mE0118269A4909CF62DAF24DC555FD5E1CC838BF3,
+	NewBehaviourScript__ctor_m437970EA37D66BDF32972F4CC0F65B95E5961FAA,
+	U3CStartSpawningU3Ed__3__ctor_m8695317A420747B040FF72F7E1AE285F1FB45CE1,
+	U3CStartSpawningU3Ed__3_System_IDisposable_Dispose_mF89F63A9106D520B6A6A3DAFC0F8EF402A11451A,
+	U3CStartSpawningU3Ed__3_MoveNext_m8BED59DD8ABF505C9A4886C7EE42A474EF6C8878,
+	U3CStartSpawningU3Ed__3_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mCE9165AFAC7075C14B1BDD2F3051E4BFA04414C8,
+	U3CStartSpawningU3Ed__3_System_Collections_IEnumerator_Reset_mC4AE2C0BEC87E85386558AF30BFC67AB24A4B606,
+	U3CStartSpawningU3Ed__3_System_Collections_IEnumerator_get_Current_m748B28AA1E9B5BC69E1AD65379123EFB95F646C8,
+	Player_Start_mBD692B64AAC791B93A589E7D3596F36787EAF021,
+	Player_Update_mBA04F1D6FE3C18037EA95DFAEEAA9977BFD49CD3,
+	Player__ctor_mDE8EB5B351975D4E7E24DE341B8B49B8A29CC2B7,
+	ShootScript_Shoot_mFF81D242F47F7059D0BFFA17D0C824C3649A5602,
+	ShootScript__ctor_m9D99A2EE188B26D935ECED4A5B730EC793BA96C0,
+	alienattack_Start_m7D535320E23183DB7727479321721A549C08CF83,
+	alienattack_Startshooting_m4A6C20966B441EF8D5A8B773B177574CB0944B55,
+	alienattack__ctor_m01ACCA1F66639AF0DFA2B7176D91230BA55C2F15,
+	U3CStartshootingU3Ed__3__ctor_m15DF1F595204E9689959739C988D65C61223D28F,
+	U3CStartshootingU3Ed__3_System_IDisposable_Dispose_m34CDB6211F4104FAC4F13C14493ECF08F6035E27,
+	U3CStartshootingU3Ed__3_MoveNext_m4BF8AAA5CFE4F3406B95B534EA0F32A018DE61E2,
+	U3CStartshootingU3Ed__3_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m79CCE0C7F3425D447A39F9AE793908B79AA31813,
+	U3CStartshootingU3Ed__3_System_Collections_IEnumerator_Reset_m870DF1A140BBA5FD81DFE27A0AA567C4DC341433,
+	U3CStartshootingU3Ed__3_System_Collections_IEnumerator_get_Current_m1F4F6EE6806670FC7D576804FA3A0C0EC0BF1AB9,
+	balloonScript_Start_m20282F79FCA5561E8E7CEBEECA98F354E5A3491A,
+	balloonScript_Update_mFB0AB5309CBA7BED8D8AD34B39499217E71BA094,
+	balloonScript__ctor_m706012D88FF5C005F9AAE2313E5DD0BE4800BA27,
+	fightingbullet_Start_mAEC96BE9CCD048A9CF914BB2DC9ECBE7D1430CE3,
+	fightingbullet_Update_mCBDA5295DC4EF361D2C655F6AC91D2A8C8D3DF8D,
+	fightingbullet__ctor_m8BAF23C795D61AC68C64F94C6C85DD8608C6A1C3,
+	ReadmeStarSparrow__ctor_m81EE107F0BDBF341ABE7355F3A3C5CA3E5983209,
+	Section__ctor_mA5437646B1723B160F8C957D956245BF25C3D7E4,
+};
+static const int32_t s_InvokerIndices[40] = 
+{
+	2066,
+	2066,
+	2066,
+	2066,
+	2066,
+	2066,
+	2066,
+	2066,
+	2066,
+	2066,
+	2029,
+	2066,
+	1697,
+	2066,
+	2051,
+	2029,
+	2066,
+	2029,
+	2066,
+	2066,
+	2066,
+	2066,
+	2066,
+	2066,
+	2029,
+	2066,
+	1697,
+	2066,
+	2051,
+	2029,
+	2066,
+	2029,
+	2066,
+	2066,
+	2066,
+	2066,
+	2066,
+	2066,
+	2066,
+	2066,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	40,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
